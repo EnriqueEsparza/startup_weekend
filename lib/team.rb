bc7361398dd2652dbf842cd31891536e37b26e01
Enrique <iqueart@yahoo.com>
2015-06-05 10:17:31 -0700

    pass all unit tests for Team class

--- lib/team.rb
+++ lib/team.rb
@@ -17,6 +17,28 @@ class Team
     @team_members
   end
 
+  define_method(:save) do
+    @@teams.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@teams
+  end
+
+  define_singleton_method(:clear) do
+    @@teams = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_team = nil
+    @@teams.each() do |team|
+      if team.id().eql?(id)
+        found_team = team
+      end
+    end
+    found_team
+  end
+
 
 
 
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,9 +3,9 @@ require('team')
 require ('member')
 
 describe(Team) do
-  # before() do
-  #   Team.clear()
-  # end
+  before() do
+    Team.clear()
+  end
 
   describe("#name") do
     it("returns the name of the team") do
@@ -27,6 +27,39 @@ describe(Team) do
       expect(test_team.team_members()).to(eq([]))
     end
   end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Team.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a team to an array of saved teams") do
+      test_team = Team.new("Wildcats")
+      test_team.save
+      expect(Team.all()).to(eq([test_team]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved teams") do
+      Team.new("Wildcats").save()
+      Team.clear()
+      expect(Team.all()).to(eq([]))
+    end
+  end
+
+  describe(".find") do
+    it("find a team based it's matching id") do
+      test_team1 = Team.new("Wildcats")
+      test_team1.save
+      test_team2 = Team.new("Mustangs")
+      test_team2.save
+      expect(Team.find(test_team2.id)).to(eq(test_team2))
+    end
+  end
+
 
 
 
