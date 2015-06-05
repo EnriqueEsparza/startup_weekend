46ae9b6eab44dd220ac1029b9c31b3382d97b148
Enrique <iqueart@yahoo.com>
2015-06-05 09:43:37 -0700

    pass first 3 specs

--- lib/team.rb
+++ lib/team.rb
@@ -1,12 +1,20 @@
 class Team
   attr_reader (:name)
-  #@@teams = []
+  @@teams = []
 
   define_method(:initialize) do |name|
     @name = name
-    #@id = @@teams.length().+(1)
-    # @team_members = []
+    @id = @@teams.length().+(1)
+    @team_members = []
 
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_method(:team_members) do
+    @team_members
   end
 
 
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -14,12 +14,19 @@ describe(Team) do
     end
   end
 
-  # describe("#id") do
-  #   it("returns the id of the team") do
-  #     test_team = Team.new("Wildcats")
-  #     expect(test_team.id()).to(eq(1))
-  #   end
-  # end
+  describe("#id") do
+    it("returns the id of the team") do
+      test_team = Team.new("Wildcats")
+      expect(test_team.id()).to(eq(1))
+    end
+  end
+
+  describe("#team_members") do
+    it("initially returns an empty array of team members") do
+      test_team = Team.new("Wildcats")
+      expect(test_team.team_members()).to(eq([]))
+    end
+  end
 
 
 
