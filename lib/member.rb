6dfcbade2b42c30f62aded3bcff3b84b082d3053
Enrique <iqueart@yahoo.com>
2015-06-05 11:24:18 -0700

    pass first six unit test for Member class

--- lib/member.rb
+++ lib/member.rb
@@ -1,16 +1,31 @@
 class Member
-  attr_reader(:name)
+  attr_reader(:name, :skill, :years)
   @@members = []
 
 
-  define_method(:initialize) do |name|
+  define_method(:initialize) do |name, skill, years|
     @name = name
-    # @skill = skill
-    # @exp = years_of_experience
-    # @id = @@members.length().+(1)
-
-
+    @skill = skill
+    @years = years
+    @id = @@members.length().+(1)
   end
+
+  define_method(:save) do
+    @@members.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@members
+  end
+
+  define_singleton_method(:clear) do
+    @@members = []
+  end
+
+
+  # define_method(:id) do
+  #   @id
+  # end
 
 
 
--- spec/member_spec.rb
+++ spec/member_spec.rb
@@ -3,16 +3,61 @@ require('team')
 require ('member')
 
 describe(Member) do
-  # before() do
-  #   Team.clear()
-  # end
+  before() do
+    Member.clear()
+  end
 
   describe("#name") do
     it("returns the name of a member") do
-      test_member = Member.new("Jon Doe")
+      test_member = Member.new("Jon Doe", "Ruby", 3)
       expect(test_member.name()).to(eq("Jon Doe"))
     end
   end
+
+  describe("#skill") do
+    it("returns the skill/software a member can contribute") do
+      test_member = Member.new("Jon Doe", "Ruby", 3)
+      expect(test_member.skill()).to(eq("Ruby"))
+    end
+  end
+
+  describe("#years") do
+    it("returns the number of years member has experience with skill/software") do
+      test_member = Member.new("Jon Doe", "Ruby", 3)
+      expect(test_member.skill()).to(eq("Ruby"))
+    end
+  end
+
+  describe("#save") do
+    it("adds a member to the array of saved member") do
+      test_member = Member.new("Jon Doe", "Ruby", 3)
+      test_member.save()
+      expect(Member.all()).to(eq([test_member]))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Member.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved members") do
+      Member.new("Jon Doe", "Ruby", 3).save()
+      Member.clear()
+      expect(Member.all()).to(eq([]))
+    end
+  end
+
+
+  # describe("#id") do
+  #   it("returns the id of the member") do
+  #     test_member = Member.new("Jon Doe", "Ruby", 3)
+  #     test_member.save
+  #     expect(test_member.id()).to(eq(1))
+  #   end
+  # end
 
 
 
